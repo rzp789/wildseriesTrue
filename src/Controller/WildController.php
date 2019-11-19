3714fae982ba94d8c59263957e37abed9fd64333
Adrien Perez <adrien.perez789@gmail.com>
2019-11-19 15:23:18 +0100

    quête 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -11,10 +11,29 @@ class WildController extends AbstractController
     /**
      * @Route("/wild", name="wild_index")
      */
-    public function index() :Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+    // requirements={"slug"="[^ABCDEFGHIJKLMNOPQRSTUVWXYZ]|[^_]"},
+
+    /**
+     * @Route("/wild/show/{slug}",defaults = {"slug":""},
+     *     methods={"GET"}, name="wild_show")
+     */
+    public function show($slug): Response
+    {
+
+        if (preg_match('/[A-Z]|[_]/', $slug)) {
+            $error = "Erreur 404";
+            return $this->render('wild/show.html.twig', ['slug' => $error]);
+        } else {
+            $truc = ["^", "$", "\\", "|", "{", "}", "[", "]", "(", ")", "?", "#", "!", "+", "*", "_", "-"];
+            $slugy = ucwords(str_replace($truc, " ", $slug));
+            return $this->render('wild/show.html.twig', ['slug' => $slugy]);
+
+        }
     }
 }
