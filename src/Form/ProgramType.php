ad808b3871c3b36dda01a1b9ea07aba2c2bad5ef
Adrien Perez <adrien.perez789@gmail.com>
2020-02-17 16:50:24 +0100

    finish

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -5,10 +5,15 @@ namespace App\Form;
 use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Entity\Actor;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotEqualTo;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class ProgramType extends AbstractType
 {
@@ -16,7 +21,8 @@ class ProgramType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('summary')
+            ->add('summary', TextareaType::class, ['constraints' =>
+                [new Regex(['pattern' => '/plus belle la vie/','match' => false, 'message' => 'On parle de vraies séries ici'])]])
             ->add('poster')
             ->add('category',null,['choice_label' => 'name'])
             ->add('actors', EntityType::class, [
